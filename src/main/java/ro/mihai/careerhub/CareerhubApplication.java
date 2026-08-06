678b7f3aee537ab2211133e43fdd5c5d9db0315c
Mihai Oprea <mihairobert05@yahoo.com>
2026-08-06 23:50:24 +0300

    First version of the Spring Project

---
+++ src/main/java/ro/mihai/careerhub/CareerhubApplication.java
@@ -0,0 +1,13 @@
+package ro.mihai.careerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mihai/careerhub/CareerhubApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mihai.careerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
